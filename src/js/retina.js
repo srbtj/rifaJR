435db54ad5d35e9dca64b58e2cd40a019ea85f42
srbtj <srbtj@aliyun.com>
2016-10-11 19:08:57 +0800

    second init

---
+++ src/js/retina.js
@@ -0,0 +1,57 @@
+(function () {
+
+    var seperator = (typeof retinaImageSeperator == 'undefined') ? '@' : retinaImageSeperator;
+
+    var mediaQuery = "(min--moz-device-pixel-ratio: 1.5),\
+                    (-o-min-device-pixel-ratio: 3/2),\
+                    (-webkit-min-device-pixel-ratio: 1.5),\
+                    (min-device-pixel-ratio: 1.5),\
+                    (min-resolution: 144dpi),\
+                    (min-resolution: 1.5dppx)";
+
+    if (window.matchMedia(mediaQuery).matches) {
+
+        var images = document.getElementsByTagName('img');
+
+        // Loop through the imgs.
+        for (var i = 0; i < images.length; i++) {
+
+            var classes = images[i].className.split(' ');
+
+            for (key in classes) {
+
+                if (classes[key] == 'hi-res') {
+
+                    // First check and removed any ? or # at end of src path.
+                    var questionPosition = images[i].src.lastIndexOf('?');
+                    var poundPosition = images[i].src.lastIndexOf('#');
+                    var periodPosition = images[i].src.lastIndexOf('.');
+
+                    if (questionPosition > periodPosition) {
+
+                        images[i].src = images[i].src.substr(0, questionPosition);
+
+                    }
+
+                    if (poundPosition > periodPosition) {
+
+                        images[i].src = images[i].src.substr(0, poundPosition);
+
+                    }
+
+                    var imageType = images[i].src.substr(-4);
+                    var imageName = images[i].src.substr(0, images[i].src.length - 4);
+                    imageName += seperator + "2x" + imageType;
+
+                    // Rename image.
+                    images[i].src = imageName;
+
+                }
+
+            }
+
+        }
+
+    }
+
+})();
