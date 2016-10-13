957496369f2ef3243bc68787218a09607ab20f07
srbtj <srbtj@aliyun.com>
2016-10-13 17:32:02 +0800

    日发

---
+++ src/js/rfNoticeListDetail.js
@@ -0,0 +1,12 @@
+($(function(){
+    /** 点击切换 **/
+    changeTap($('.nav-left'),'a');
+    changeTap($('.header-center-item'),'li');
+    changeTap($('.language'),'a');
+    changeTap($('.notice-list'),'li');
+    function changeTap(obj,opt){
+        obj.on('click',opt,function(e){
+            $(this).addClass('active').siblings().removeClass('active');
+        })
+    }
+}));
