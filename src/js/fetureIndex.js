273672fa7c9b7757f5d57a0e375b0c4bc21588a8
srbtj <srbtj@aliyun.com>
2016-10-18 17:47:19 +0800

    首页及期货页面

--- src/js/fetureIndex.js
+++ src/js/fetureIndex.js
@@ -4,11 +4,24 @@
     changeTap($('.header-center-item'),'li');
     changeTap($('.language'),'a');
     changeTap($('.banner-nav-text'),'a');
-    changeTap($('#feture-warp .feture-nav'),'a');
+    //changeTap($('#feture-warp .feture-nav'),'a');
     changeTap($('#feture-warp .feture-second-nav'),'a');
+
+    changeTap($('.transfDemo-table .left-demo ul'),'li');
     function changeTap(obj,opt){
         obj.on('click',opt,function(e){
             $(this).addClass('active').siblings().removeClass('active');
         })
     }
+
+    $('#feture-warp .feture-nav').find('a').each(function(i,item){
+        var that = $(this);
+        that.on('click',function(){
+            var index = that.index();
+            that.addClass('active').siblings().removeClass('active');
+            $('#feture-warp .fetureBanner-table > div').eq(index).show()
+                .siblings().hide();
+        })
+    });
+
 }));
