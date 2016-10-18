3d88a082e190371f6caa272e893afecc57921390
srbtj <srbtj@aliyun.com>
2016-10-18 18:42:38 +0800

    使用rem实现响应式布局

--- src/js/test.js
+++ src/js/test.js
@@ -16,3 +16,21 @@
 //
 
 //}
+
+(function (doc, win, undefined) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in win? 'orientationchange' : 'resize',
+        recalc = function () {
+            var clientWidth = docEl.clientWidth;
+
+            if(clientWidth > 750)  return;
+            console.log('------>>>>',clientWidth);
+            if (clientWidth === undefined) return;
+            docEl.style.fontSize = 50 * (clientWidth / 750) + 'px';
+        };
+    if (doc.addEventListener === undefined) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false)
+})(document, window);
+
+
