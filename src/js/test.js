15ad33cca0fd06d4f2759709e4517cb540ac8db6
srbtj <srbtj@aliyun.com>
2016-10-18 09:24:29 +0800

    手机端

---
+++ src/js/test.js
@@ -0,0 +1,18 @@
+//window.onresize = function(){
+//    console.log(11)
+//
+//
+//    var oHtml = document.querySelector('html');
+//
+//    console.log(oHtml);
+//
+//    var width = oHtml.getBoundingClientRect().width;
+//
+//
+//    oHtml.style.fontSize = width / 50 + 'px';
+//
+//
+//
+//
+
+//}
