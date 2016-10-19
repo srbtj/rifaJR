ae740856ffde1ec10278011b294d6b6667643a15
srbtj <srbtj@aliyun.com>
2016-10-20 07:59:14 +0800

    页面微调

--- src/js/rifaIndex.js
+++ src/js/rifaIndex.js
@@ -15,5 +15,37 @@
         var title = $(this).attr('data-title');
         //console.info(title);
         $('#rifa-wrap .refa-banner-txt').html(title);
-    })
+    });
+
+
+    changeStyle();
+
+    function changeStyle(){
+        var searchs = location.search.replace('?','');
+
+        if(!searchs) return;
+
+        var temp_arr = searchs.split('&');
+
+        var temp_obj = {};
+
+        for(var i= 0,len = temp_arr.length;i<len;i++){
+
+            var temp = temp_arr[i];
+
+            var arr = temp.split('=');
+
+            temp_obj[arr[0]] = arr[1];
+        }
+
+        var tVal = temp_obj.tab || 0;
+
+        $('ul[role=tablist]').find('li').eq(tVal)
+            .addClass('active').siblings().removeClass('active');
+
+        $('.rifa-item > div').eq(tVal).addClass('active').siblings().removeClass('active');
+
+        var text = $('ul[role=tablist]').find('li').eq(tVal).attr('data-title');
+        $('.rifa-title > span').text(text);
+    }
 }));
